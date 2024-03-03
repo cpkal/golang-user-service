2595e3096f2883a55cb8bf430f94d5b652ba3f9d
cpkal <haikal.putra.gustiansyah@upi.edu>
2024-03-04 01:57:03 +0700

    Bash run server

--- cmd/user-service/main.go
+++ cmd/user-service/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"net/http"
+	"time"
 
+	"github.com/cpkal/golang-user-service/api/models"
 	"github.com/cpkal/golang-user-service/internal/repository"
 	"github.com/cpkal/golang-user-service/internal/service"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -15,5 +19,28 @@ func main() {
 
 	userService.GetUserByID(context.Background(), "1")
 
-	//startServer()
+	r := gin.Default()
+
+	r.GET("/test", func(c *gin.Context) {
+		var user models.User
+		user.ID = "1"
+		user.Email = "dummy@dummy.com"
+		user.Password = "supersecret"
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+
+		err := userService.CreateUser(c, &user)
+
+		c.JSON(http.StatusOK, gin.H{
+			"error": err,
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
---
+++ run_dev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+go run ./cmd/user-service/main.go
